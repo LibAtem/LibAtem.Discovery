dd7cb52150dc46fbc10c2efe3295ebf681cd9861
Julian Waller <git@julusian.co.uk>
2019-04-25 17:31:43 +0100

    Initial commit

---
+++ LibAtem.Discovery.Simple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibAtem.Discovery.Simple
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var discovery = new AtemDiscoveryService(10000, true);
+            discovery.OnDeviceSeen += (s, d) =>
+            {
+                Console.WriteLine("Seen: " + d.ToString());
+            };
+            discovery.OnDeviceLost += (s, d) =>
+            {
+                Console.WriteLine("Lost: " + d.ToString());
+            };
+
+            Console.Read();
+        }
+    }
+}
